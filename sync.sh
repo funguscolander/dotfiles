2b9655f4e84dafa1c5833cb38d3a148d061bef14
fergus <59059263+C-Fergus@users.noreply.github.com>
2020-06-07 00:55:19 +0100

    Create sync.sh

---
+++ sync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -svf ~/dotfiles/.bash_aliases ~
+ln -svf ~/dotfiles/.bashrc ~
+ln -svf ~/dotfiles/.gitconfig ~
+ln -svf ~/dotfiles/.profile ~
+ln -svf ~/dotfiles/.vimrc ~
+ln -svf ~/dotfiles/.Xmodmap ~
