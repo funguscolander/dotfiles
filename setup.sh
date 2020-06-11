ac8f469dbb46f1526809ba204e93a4cfdbe0a39d
fergus <59059263+C-Fergus@users.noreply.github.com>
2020-06-11 22:01:49 +0100

    Create WSLlocalsshsetup. Add binaries to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-bash sync.sh
+# bash sync.sh
+
+main() {
+
+  local i=''
+
+  # Copy BINARIES
+  ln -svf $HOME/dotfiles/bin $HOME
+
+  declare -a BINARIES=(
+    'getsshkey'
+    'WSLlocalsshsetup'
+  )
+
+  for i in ${BINARIES[@]}; do
+    echo "Changing access permissions for binary script :: ${i##*/}"
+    chmod +rwx $HOME/bin/${i##*/}
+  done
+
+  unset BINARIES
+
+}
+
+main
+
